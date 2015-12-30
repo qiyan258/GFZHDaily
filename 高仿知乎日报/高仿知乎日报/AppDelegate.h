df372833df7256d36cfc115390e554fe6da6ce44
M了个M <847881570@qq.com>
2015-12-30 14:44:03 +0800

    - init

---
+++ 高仿知乎日报/高仿知乎日报/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  高仿知乎日报
+//
+//  Created by 周发明 on 15/12/30.
+//  Copyright © 2015年 周发明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 高仿知乎日报/高仿知乎日报/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  高仿知乎日报
+//
+//  Created by 周发明 on 15/12/30.
+//  Copyright © 2015年 周发明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
